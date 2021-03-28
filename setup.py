bccb36a2b7bd3e4cb036e960f3108d8b43029b7a
Robert Blenis <r.blenis@visionxyz.com>
2021-03-28 05:19:04 -0400

    use setuptools_scm to generate version from git tags

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     include_package_data=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    package_data={'vorta.i18n': ['qm/*.qm']}
+    package_data={'vorta.i18n': ['qm/*.qm']},
+    use_scm_version={'write_to': 'src/vorta/_version.py', 'write_to_template': '__version__ = "{version}"\n'}
 )
